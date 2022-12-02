a90dcc534ed9362662bb1e688665c4f91a910049
hakimsy <hakimsy17@gmail.com>
2022-12-02 15:27:52 +0000

    Adding try/catch blocks for the backend

--- vid-api-master/controllers/factureCtrl.js
+++ vid-api-master/controllers/factureCtrl.js
@@ -2,10 +2,17 @@ const db = require('../models');
 
 
 exports.getFacture = async (req, res, next) => {
-    const patientId = await req.params.PatientId;
-    let facture = await db.Acte.findAll({
-        where: { PatientId: patientId }
-    });
-    const total = facture.reduce((total, obj) => obj.dataValues.sousTotal + total, 0);
-    res.status(201).json({total});
+    try {
+
+        const patientId = await req.params.PatientId;
+        let facture = await db.Acte.findAll({
+            where: { PatientId: patientId }
+        });
+        const total = facture.reduce((total, obj) => obj.dataValues.sousTotal + total, 0);
+        res.status(201).json({total});
+    } catch (err) {
+        res.status(500).send({
+            error: err
+        });
+    }
 };
--- vid-api-master/controllers/patientCtrl.js
+++ vid-api-master/controllers/patientCtrl.js
@@ -1,19 +1,31 @@
 const db = require('../models');
 
 exports.createPatient = async (req, res, next) => {
-    const patient = await db.Patient.create({
-        nom: req.body.nom,
-        prenom: req.body.prenom,
-        naissance: req.body.naissance,
-        organisme: req.body.organisme,
-        age: req.body.age,
-        sexe: req.body.sexe,
-        nationalite: req.body.nationalite,
-        dateEntree: req.body.dateEntree,
-    }).then((patient) => res.status(201).json({ patient }));
-    console.log(patient);
+    try {
+        const patient = await db.Patient.create({
+            nom: req.body.nom,
+            prenom: req.body.prenom,
+            naissance: req.body.naissance,
+            organisme: req.body.organisme,
+            age: req.body.age,
+            sexe: req.body.sexe,
+            nationalite: req.body.nationalite,
+            dateEntree: req.body.dateEntree,
+        }).then((patient) => res.status(201).json({ patient }));
+        console.log(patient);
+    } catch (err) {
+        res.status(500).send({
+            error: err
+        });
+    }
 };
 
 exports.getAllPatients = async (req, res, next) => {
-     await db.Patient.findAll().then((patients) => res.send(patients));
+    try {
+        await db.Patient.findAll().then((patients) => res.send(patients));
+    } catch (err) {
+        res.status(500).send({
+            error: err
+        });
+    }
 };
